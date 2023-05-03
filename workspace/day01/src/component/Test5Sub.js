4a8d8342c96af098b5d8ec84ce08d3ac1dd07378
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-03 18:00:51 +0900

    05.03.18

--- workspace/day01/src/component/Test5Sub.js
+++ workspace/day01/src/component/Test5Sub.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Test5Sub = () => {
+const Test5Sub = ({name, age, addr, tel, color, bgcolor, done}) => {
     return (
         <div>
-            
+            <h2>신상명세서</h2>
+            <ul style={{ color: color, backgroundColor: bgcolor }}>
+                <li>이름 : { name }</li>
+                <li>나이 : { age }</li>
+                <li>핸드폰 : { tel }</li>
+                <li>주소 : { addr }</li>
+                <li>동의 여부 : { done ? '동의' : '비동의' }</li>
+            </ul>
         </div>
     );
 };
