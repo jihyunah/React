c32e08dbce8ef210f84b8c386812659d4e72fcfa
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-09 14:46:38 +0900

    05.09

--- workspace/day03/src/App.js
+++ workspace/day03/src/App.js
@@ -3,6 +3,7 @@ import Test01 from './component/Test01.js';
 import Test02 from './component/Test02.js';
 import Test03 from './component/Test03.js';
 import Test04 from './component/Test04.js';
+import Test05 from './component/Test05.js';
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       {/* <Test01 /> */}
       {/* <Test02 /> */}
       {/* <Test03 /> */}
-      <Test04 />
+      {/* <Test04 /> */}
+      <Test05 />
     </div>
   );
 };
