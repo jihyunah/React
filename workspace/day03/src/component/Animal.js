c33f3673b94323996c42754aaf4bdddabf343971
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-09 14:37:04 +0900

    05.09

--- workspace/day03/src/component/Animal.js
+++ workspace/day03/src/component/Animal.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Animal = () => {
+const Animal = ({ name, onInputName }) => {
     return (
         <div>
-            
+            <h2>Animal 컴포넌트</h2>
+            <label>동물 이름을 입력하시오</label> &emsp;
+            <input type="text" value={name} onChange={ onInputName }/>
         </div>
     );
 };
--- workspace/day03/src/component/Test04.js
+++ workspace/day03/src/component/Test04.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import Reac, {useState} from 'react';
+import Animal from './Animal.js';
 
 const Test04 = () => {
+    const [name, setName] = useState('호랑이')
+
+    const onInputName = (e) => {
+        const {value} = e.target;
+        setName(value);
+    }
     return (
         <div>
-            
+            <Animal name={ name } onInputName={ onInputName }/>
         </div>
     );
 };
