29f9cde0b265c65962645b4e3514b71133dec00e
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-12 12:25:38 +0900

    05.12

--- workspace/day06/src/component/Test01.js
+++ workspace/day06/src/component/Test01.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+const initialState = 0 //초기값 잡기 
+
+//함수
+const reducer = (state, action) => {
+    switch(action.type){ //action의 타입으로 나눠주기
+        case 'INCREMENT' : 
+            return state + 1
+        case 'DECREMENT' : 
+            return state - 1
+        case 'RESET' : 
+            return 0
+        default: //default는 반드시 작성해야 한다.
+            return false
+    }
+}
 
 const Test01 = () => {
-    const [count, setCount] = useReducer()
+    //count -> state 에게 전달되고, dispatch -> action으로 전달된다
+    //reducer 함수는 state, action을 받아서 새로운 state 함수를 반환한다.
+    const [count, dispatch] = useReducer(reducer, initialState)
 
     return (
         <div>
-            
+            <h1>카운트 : { count }</h1>
+            <p>
+                <button onClick={ () => dispatch({ type: 'INCREMENT'}) }>증가</button>
+                <button onClick={ () => dispatch({ type: 'DECREMENT'}) }>감소</button>
+                <button onClick={ () => dispatch({ type: 'RESET'}) }>초기화</button>
+            </p>
         </div>
     );
 };
