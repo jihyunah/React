af13b4bae89a2b92aafdfe76ef7db8fb0a92cbff
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-15 12:47:26 +0900

    05.15

--- workspace/day07/src/page04/MemberDetail.js
+++ workspace/day07/src/page04/MemberDetail.js
@@ -1,12 +1,33 @@
+import axios from 'axios';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const MemberDetail = () => {
     const {memberId} = useParams()
-    
+
+    const[data, setData] = useState([])
+
+    //데이터 가져오기 
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+             .then(res => setData(res.data))
+    }, []) //버튼을 눌렀을 때 마다 
+
+    const css = {
+        border: '1px solid cyan', margin: 20, padding: 20
+    }
+
     return (
-        <div>
-            
+        <div style={ css }>
+            <h2>MemberDetail Page: { memberId }</h2>
+            <h4>이름 : { data.name }</h4>
+            <ul>
+                <li>이메일 : { data.email }</li>
+                <li>전화번호 : { data.phone }</li>
+                <li>웹사이트 : { data.website }</li>
+            </ul>
         </div>
     );
 };
