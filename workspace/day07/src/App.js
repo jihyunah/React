19c5b2720c4189cefb3710c9888f72d4122ced96
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-15 10:10:40 +0900

    05.15

--- workspace/day07/src/App.js
+++ workspace/day07/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import App01 from './App01';
 
 const App = () => {
   return (
     <div>
-      
+      <App01 />
     </div>
   );
 };
--- workspace/day07/src/App01.js
+++ workspace/day07/src/App01.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App01 = () => {
     return (
-        <div>
-            
-        </div>
+        <BrowserRouter>
+            <>
+                <nav>
+
+                </nav>
+
+                {/* 화면에 보이는 영역 */}
+                <Routes>
+                    <Route path='/'></Route>
+                    <Route path='/about'></Route>
+                    <Route path='/ceo'></Route>
+                    <Route path='/sub01'></Route>
+                </Routes>
+            </>
+        </BrowserRouter>
     );
 };
 
