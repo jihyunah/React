cc22a96f16e031c2ce6c1e28ea8691ce2edde141
이지현 <121652350+jihyunah@users.noreply.github.com>
2023-05-15 11:12:26 +0900

    05.15

--- workspace/day07/src/App02.js
+++ workspace/day07/src/App02.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import About from './page02/About';
+import Ceo from './page02/Ceo';
+import Home from './page02/Home';
+import NotFiles from './page02/NotFiles';
+import Sub01 from './page02/Sub01';
+import NavBar from './page02/NavBar';
 
 const App02 = () => {
     return (
-        <div>
-            
-        </div>
+        <BrowserRouter>
+            <>
+                <NavBar />
+
+                {/* 화면에 보이는 영역 */}
+                <Routes>
+                    <Route path='/' element={ <Home /> }/>
+                    <Route path='/about' element={ <About /> }/>
+                    <Route path='/ceo' element={ <Ceo />} />
+                    <Route path='/sub01' element={ <Sub01 /> }/>
+                    <Route path='*' element={ <NotFiles /> } />
+                </Routes>
+            </>
+        </BrowserRouter>
     );
 };
 
--- workspace/day07/src/page02/NavBar.js
+++ workspace/day07/src/page02/NavBar.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import data from './NavData.js';
+import '../css/style02.css';
+import { useState } from 'react';
 
 const NavBar = () => {
+    const [isNav, setIsNav] = useState(false);
+
+    const onToggle = () => {
+        setIsNav(!isNav);
+    }
+
     return (
-        <div>
-            
+        <div className='navbar'>
+            <p className='all-menu' onClick={ onToggle }>menu</p>
+            <nav className={ isNav ? 'on' : ''}> {/* isNav가 true면 on 클래스를 부여, false면 클래스 속성 부여 X */} 
+                <ul>
+                    {
+                    data.map((item, index)=> <li key={ index }>
+                        <Link to={ item.path }>{ item.title }</Link>
+                    </li>)
+                    }
+
+                    {/*
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/ceo'>Ceo</Link></li>
+                    <li><Link to='/sub01'>Sub01</Link></li>
+                    */}
+                </ul>
+            </nav>
         </div>
     );
 };
