8f6648c31a390bff29ec87f22a18767af98c1d16
jihyunah <wisedew2@gmail.com>
2023-06-05 10:58:02 +0900

    06.05

--- workspace/day08/store/modules/color.js
+++ workspace/day08/store/modules/color.js
@@ -1,11 +1,31 @@
-import React from 'react';
+// 1. 액션 생성
+const RED = 'color/RED'
+const GREEN = 'color/GREEN'
+const BLUE = 'color/BLUE'
+const TOMATO = 'color/TOMATO'
 
-const color = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+// 2. 액션 내보내기
+export const red = () => ({ type: RED })
+export const green = () => ({ type: GREEN })
+export const blue = () => ({ type: BLUE })
+export const tomato = () => ({ type: TOMATO })
 
-export default color;
+// 3. 초기값
+const initialState = { color: 'hotpink' }
+
+// 4. 리듀서 만들기 - state, action 파라미터를 참조하여, 새로운 상태 객체를 만들어서 반환한다.
+// state에는 반드시 초기값을 주어야 한다.
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case RED:
+            return { color: 'red' }
+        case GREEN:
+            return { color: 'green' }
+        case BLUE:
+            return { color: 'blue' }
+        case TOMATO:
+            return { color: 'tomato' }
+        default
+    }
+} //state: 현재 상태, action: 액션 객체
+
