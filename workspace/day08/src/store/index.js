11e7edf8d8d2c35bbf60f51c294922b43da585cd
jihyunah <wisedew2@gmail.com>
2023-06-05 11:39:59 +0900

    06.05

--- workspace/day08/src/store/index.js
+++ workspace/day08/src/store/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import color from './modules/color.js';
+import count from './modules/count.js';
 
 export default combineReducers({
-    color // color : color
+    color, // color : color
+    count, // count: count
 })
